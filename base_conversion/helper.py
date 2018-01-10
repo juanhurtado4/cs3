5cb900efb57ecf1486e31a879211c67814bb939d
juan hurtado <juanhurtado4@outlook.com>
2018-01-10 01:24:03 -0800

    Feat: Add helper module for convert_bases

---
+++ base_conversion/helper.py
@@ -0,0 +1,23 @@
+def get_exponents(digit_str):
+    digit_length = len(digit_str) - 1
+    exponents = []
+    for number, digit in zip(range(digit_length, -1, -1), digit_str):
+        if digit == '1':
+            exponents.append(number)
+    return exponents
+
+
+
+if __name__ == '__main__':
+    digits = {
+        202: '11001010',
+        129: '10000001',
+        250: '11111010',
+        195: '11000011',
+        166: '10100110',
+        7: '111'
+    }
+    for answer, digits in digits.items():
+        exponents = get_exponents(digits)
+        result = sum([2 ** expo for expo in exponents])
+        assert result == answer 
