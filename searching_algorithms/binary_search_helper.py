c178747df87d822a6525e56c72f67cb6a8b3fa25
juan hurtado <juanhurtado4@outlook.com>
2018-01-12 00:44:21 -0800

    Docs: Add docs for all functions

--- searching_algorithms/binary_search_helper.py
+++ searching_algorithms/binary_search_helper.py
@@ -1,21 +1,10 @@
 def is_empty(arr):
-    '''
-    Arr: Array
-    Checks if an array is empty
-    Returns Boolean
-    '''
+    '''Checks if an array is empty'''
     return True if len(arr) < 1 else False
 
-
 def are_items_equal(arr_item, item):
-    '''
-    Arr_item: any
-    Item: any
-    Checks if array item is the same as item
-    Returns boolean
-    '''
+    '''Checks if array item is the same as item. Returns boolean'''
     return True if arr_item == item else False
-
 
 def is_middle_value_bigger(middle_item, target):
     '''
@@ -36,10 +25,13 @@ def is_middle_value_bigger(middle_item, target):
     return True if middle_item[0] > target[0] else False
 
 def get_start_position(middle_ind):
+    '''Calculates the starting position. Returns Int'''
     return int(middle_ind + 1)
 
 def get_middle_position(start_ind, end_ind):
+    '''Calculates the middle position. Returns Int'''
     return int((end_ind + start_ind) / 2)
 
 def get_end_position(middle_ind):
+    '''Calculates the ending position. Returns Int'''
     return int(middle_ind - 1)
