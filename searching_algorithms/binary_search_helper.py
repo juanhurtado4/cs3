b615b035134057d694b0d43286cca0ef293a6809
juan hurtado <juanhurtado4@outlook.com>
2018-01-11 23:56:42 -0800

    Refactor: Modularize binary_iterative by creating helper func

---
+++ searching_algorithms/binary_search_helper.py
@@ -0,0 +1,45 @@
+def is_empty(arr):
+    '''
+    Arr: Array
+    Checks if an array is empty
+    Returns Boolean
+    '''
+    return True if len(arr) < 1 else False
+
+
+def are_items_equal(arr_item, item):
+    '''
+    Arr_item: any
+    Item: any
+    Checks if array item is the same as item
+    Returns boolean
+    '''
+    return True if arr_item == item else False
+
+
+def is_middle_value_bigger(middle_item, target):
+    '''
+    middle_item: string
+    target: string
+    Checks if first letter of the first item is bigger than, the second item first letter
+    Returns boolean
+    '''
+    if middle_item[0] == target[0]:
+        for middle_char, target_char in zip(middle_item, target):
+            # Check each char until non duplicate is found
+            if middle_char != target_char:
+                return True if middle_char > target_char else False
+        # if duplicate return true
+        return True
+
+
+    return True if middle_item[0] > target[0] else False
+
+def get_start_position(middle_ind):
+    return int(middle_ind + 1)
+
+def get_middle_position(start_ind, end_ind):
+    return int((end_ind + start_ind) / 2)
+
+def get_end_position(middle_ind):
+    return int(middle_ind - 1)
