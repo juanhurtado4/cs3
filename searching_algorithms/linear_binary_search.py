fab1bdcb3f87aae017f3137bd3bd8e2623e8b15b
juan hurtado <juanhurtado4@outlook.com>
2018-01-12 00:02:27 -0800

    Chore: Finish linear_search_iterative

--- searching_algorithms/linear_binary_search.py
+++ searching_algorithms/linear_binary_search.py
@@ -1,22 +1,4 @@
-def is_empty(arr):
-    '''
-    Arr: Array
-    Checks if an array is empty
-    Returns Boolean
-    '''
-    return True if len(arr) < 1 else False
-
-def are_items_equal(arr_item, item):
-    '''
-    Arr_item: any
-    Item: any
-    Checks if array item is the same as item
-    Returns boolean
-    '''
-    return True if arr_item == item else False
-
-def is_first_value_bigger(first_item, second_item):
-    return True if first_item[0] > second_item[0] else False
+from binary_search_helper import *
 
 def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
@@ -40,7 +22,7 @@ def linear_search_recursive(array, item, index=0):
 
     if index == len(array):
         return None
-    elif items_are_equal(array[index], item):
+    elif are_items_equal(array[index], item):
         return index
     # elif array[index] == item:
     #     return index
@@ -59,20 +41,37 @@ def binary_search(array, item):
 def binary_search_iterative(array, item):
     if is_empty(array):
         return None
-    elif len(array) == 1:
-        if are_items_are_equal(array[0], item):
-            return 0
-        else:
-            return None
 
     sorted_arr = sorted(array)
-    middle_ind = len(sorted_arr / 2)
 
-    while len(sorted_arr) > 1:
-        if is_first_value_bigger(sorted_arr[middle_ind], item):
+    subarr_start_position = 0
+    subarr_end_position = len(sorted_arr) - 1
+    subarr_middle_position = get_middle_position(
+                                subarr_start_position, 
+                                subarr_end_position
+                                )
 
-        ord(sorted_arr[middle_ind])
+    while subarr_start_position < subarr_end_position:
 
+        middle_item = sorted_arr[subarr_middle_position]
+
+        if are_items_equal(middle_item, item):
+            return subarr_middle_position
+
+        if is_middle_value_bigger(middle_item, item):
+            subarr_end_position = get_end_position(subarr_middle_position)
+
+        else:
+            subarr_start_position = get_start_position(subarr_middle_position)
+
+        subarr_middle_position = get_middle_position(
+                                    subarr_start_position, 
+                                    subarr_end_position
+                                    )
+
+    if are_items_equal(sorted_arr[subarr_start_position], item):
+        return subarr_start_position
+    return None
 
 def binary_search_recursive(array, item, left=None, right=None):
     # TODO: implement binary search recursively here
