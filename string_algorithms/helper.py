f2c583cf0af6855c3c47565679de659ca01c50df
juan hurtado <juanhurtado4@outlook.com>
2018-01-15 12:41:30 -0800

    Feat: Implement helper functions with descriptive names

--- string_algorithms/helper.py
+++ string_algorithms/helper.py
@@ -1,8 +1,18 @@
-def left_is_bigger(left, right):
-    return True if left > right else False
+def assign_starting_vars(text):
+    return (text.lower(), 0, len(text) -1 )
+
+def assign_chars(text, left, right):
+    return (text[left], text[right])
+
+def check_in_alphabet(char, char2):
+    char_result = True if char.isalpha() else False
+    char2_result = True if char2.isalpha() else False
+    return (char_result, char2_result)
+
+# def left_is_bigger(left, right):
+#     return True if left > right else False
+def right_is_bigger(left, right):
+    return True if left < right else False
 
 def chars_dont_match(char, char2):
     return True if char != char2 else False
-
-def assign_variables(text):
-    return (text.lower(), 0, len(text) -1 )
