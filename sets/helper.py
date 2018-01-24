67dc8e01142061fed51e072d8002bd85173a6368
juan hurtado <juanhurtado4@outlook.com>
2018-01-24 02:46:29 -0800

    Refactor: Move helper funcs into sets file

--- sets/helper.py
+++
@@ -1,8 +0,0 @@
-def add_elements_from(set1, set2):
-    for key, value in set2.set.items():
-        if not set1.contains(key):
-            set1.set(key, value)
-    return set1
-    
-
-    
--- sets/sets.py
+++ sets/sets.py
@@ -1,5 +1,4 @@
 from hashtable import HashTable
-from helper import add_elements_from
 
 class Set():
     def __init__(self, elements=None):
@@ -27,7 +26,35 @@ class Set():
         raise KeyError("Cannot remove nonexistant element")
 
     def union(self, other_set):
-        if self.get_size() == 0 and other_set.get_size() == 0:
+        if are_sets_empty(self.set, other_set):
             raise ValueError('Cannot unify empty sets')
 
-        return add_elements_from(self.set, other_set)
+        return manipulate_sets('union', self.set, other_set)
+
+    def intersection(self, other_set):
+        if are_sets_empty(self.set, other_set):
+            return self.set
+        
+        return manipulate_sets()
+
+
+
+
+def manipulate_sets(operation, set1, set2):
+
+    if operation == 'union':
+        for key, value in set2.set.items():
+            if not set1.contains(key):
+                set1.add(key, value)
+        return set1
+
+    elif operation == 'intersect':
+        new_set = Set()
+        for key, value in set2.set.items():
+            if set1.contains(key):
+                new_set.add(key, value)
+        return new_set
+
+def are_sets_empty(set1, set2):
+    sets_empty = set1.get_size() == 0 and set2.get_size() == 0
+    return True if sets_empty else False
