debe4e88006482dc7738359c547e3bec75ea437f
juan hurtado <juanhurtado4@outlook.com>
2018-01-28 18:07:17 -0800

    Test: Include failing and passing test

--- call_routing_project/test_scenarios.py
+++ call_routing_project/test_scenarios.py
@@ -1,7 +1,9 @@
 from scenario1 import get_cost
 import pytest
 
-def test_scenario1_cost_is_001():
-    assert get_cost('+15124156620') == 0
+def test_scenario1_cost_is_0():
+    assert get_cost('+15124156620', 'carrier_route1.txt') == 0
 
+def test_scenario1_cost_is_001():
+    assert get_cost('+1941613', 'carrier_route1.txt') == 0.05
 
